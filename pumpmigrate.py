04de70bc25ab4039c2608bd85a2a46dff9ff1f9a
Jonas Haraldsson <jonas227@gmail.com>
2013-07-22 23:20:11 +0200

    Now working
    README
    License block
    Code should be useable

--- pumpmigrate.py
+++ pumpmigrate.py
@@ -1,88 +1,190 @@
 #!/usr/bin/env python
+##
+# This program is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by 
+# the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version. 
+# 
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# GNU General Public License for more details. 
+# 
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
 
-import json
+
+import json, sys, os
+from datetime import datetime
 import requests
-from clint import resources as res
 from pypump import PyPump
 from requests_oauthlib import OAuth1
 
 class Account():
-    def __init__(self, client_name, webfinger, key=None, secret=None, token=None, token_secret=None):
+    def __init__(self, client_name, webfinger, key=None, secret=None,
+                 token=None, token_secret=None):
+
+        self.following = None
+
         self.client_name = client_name
         self.webfinger = webfinger
-        self.key = key
-        self.secret = secret
-        self.token = token
-        self.token_secret = token_secret
-        self.following = []
-        self.uniquefollows = []
-        self.pump = PyPump(self.webfinger, key=self.key, secret=self.secret, token=self.token, token_secret=self.token_secret, client_name=self.client_name)
+        self.pump = PyPump(self.webfinger, key=key, secret=secret,
+                           token=token, token_secret=token_secret,
+                           client_name=self.client_name)
+
+        self.key, self.secret, self.expiry = self.pump.get_registration()
+        self.token, self.token_secret = self.pump.get_token()
 
         self.getFollowing()
 
     def getFollowing(self):
-        # Grab contacts followed from account
-        if not res.cache.read('%s_at_%s_follows.json' % (self.pump.nickname, self.pump.server)):
-            url = 'https://%s/api/user/%s/following?count=200' % (self.pump.server, self.pump.nickname)
-            auth = OAuth1(self.key, self.secret, self.token, self.token_secret)
-            try:
-                following = json.loads(requests.get(url, auth=auth).content)
-            except Exception as e:
-                print 'couldnt get followers, %s' % e.message
-                return
+        self.following = []
+        url = 'https://%s/api/user/%s/following' % (self.pump.server, self.pump.nickname)
+        auth = OAuth1(self.key, self.secret, self.token, self.token_secret)
+
+        while True:
+            response = requests.get(url, auth=auth)
+            following = json.loads(response.content)
             for item in following['items']:
                 self.following.append(item['id'].split(':')[1])
-            res.cache.write('%s_at_%s_follows.json' % (self.pump.nickname, self.pump.server),
-                            json.dumps(self.following))
-        else:
-            self.following = json.loads(res.cache.read('%s_at_%s_follows.json' % (self.pump.nickname,
-                                                                                  self.pump.server)))
-        print '%s is following %s people' % (self.webfinger, len(self.following))
+            if 'next' in following['links']:
+                url = following['links']['next']['href']
+            else:
+                break
 
+    def follow(self, webfinger):
+        try:
+            # 'pypump=self.pump' is needed for now,
+            # see https://github.com/xray7224/PyPump/issues/37
+            return self.pump.Person(webfinger, pypump=self.pump).follow()
+        except:
+            return False
+
+    def unfollow(self, webfinger):
+        try:
+            return self.pump.Person(webfinger, pypump=self.pump).unfollow()
+        except:
+            return False
 
 class Client():
     def __init__(self):
         self.client_name = 'pumpmigrate'
-        self.account_names = ['old', 'new']
-        res.init('kabniel', 'pumpmigrate')
-        if res.user.read('config.json'):
-            self.cfg = json.loads(res.user.read('config.json'))
-        else:
-            self.cfg = {}
+        self.cfgFile = os.path.join(os.environ['HOME'],'.config', self.client_name,'config.json')
+        self.cacheDir = os.path.join(os.environ['HOME'],'.cache', self.client_name)
+
+        self.cfg = {}
         self.accounts = {}
+        self.account_aliases = ['old', 'new']
 
-        for acct in self.account_names:
-            self.accountSetup(acct)
+        self.loadCfg()
+        for alias in self.account_aliases:
+            self.accountSetup(alias)
 
-    def accountSetup(self, acct):
-        if acct in self.cfg:
-            cfg = self.cfg[acct]
-            self.accounts[acct] = Account(self.client_name, cfg['webfinger'],cfg['key'], cfg['secret'], cfg['token'], cfg['token_secret'])
-            print 'account %s loaded from config[%s]' % (self.cfg[acct]['webfinger'], acct)
+    def writeCfg(self):
+        if not os.path.exists(os.path.dirname(self.cfgFile)):
+            os.makedirs(os.path.dirname(self.cfgFile))
+        with open(self.cfgFile, 'w') as f:
+            f.write(json.dumps(self.cfg))
+            f.close()
+    
+    def loadCfg(self):
+        try:
+            with open(self.cfgFile, 'r') as f:
+                self.cfg = json.loads(f.read())
+                f.close()
+        except IOError:
+            return False
+        return True
+
+    def backupFollowing(self, acct):
+        tstamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        backupFile = os.path.join(self.cacheDir, "%s_following_%s.json" % (acct.webfinger, tstamp))
+        if not os.path.exists(os.path.dirname(backupFile)):
+            os.makedirs(os.path.dirname(backupFile))
+        with open(backupFile, 'w') as f:
+            f.write(json.dumps(acct.following))
+            f.close()
+
+    def accountSetup(self, alias):
+        # Prompt for webfinger
+        webfinger = raw_input('Enter webfinger (user@example.com) for your %s account: ' % alias)
+
+        if webfinger in self.cfg:
+            # Load account using stored auth info if found in config
+            cfg = self.cfg[webfinger]
+            self.accounts[alias] = Account(self.client_name,
+                                          cfg['webfinger'],
+                                          cfg['key'],
+                                          cfg['secret'],
+                                          cfg['token'],
+                                          cfg['token_secret'])
+
         else:
-            webfinger = raw_input('Enter your webfinger for your %s account: ' % acct)
-            self.accounts[acct] = Account(self.client_name, webfinger)
-            key, secret = self.accounts[acct].pump.get_registration()[:2]
-            token, token_secret = self.accounts[acct].pump.get_token()
-            cfg = {'webfinger':webfinger, 'key':key, 'secret':secret,'token':token,'token_secret':token_secret}
-            self.cfg[acct] = cfg
-            res.user.write('config.json', json.dumps(self.cfg))
+            # Load account using only webfinger (this will ask user for auth) 
+            # and write auth info to config
+            self.accounts[alias] = Account(self.client_name, webfinger)
 
-    def getUniqueFollowing(self, account_names):
-        for f in self.accounts[account_names[0]].following:
-            if f not in self.accounts[account_names[1]].following:
-                self.accounts[account_names[0]].uniquefollows.append(f)
-        print '%s is not following %s people from %s\'s list' % (self.accounts[account_names[1]].webfinger, len(self.accounts[account_names[0]].uniquefollows), self.accounts[account_names[0]].webfinger)
+            self.cfg[webfinger] = {
+                'webfinger':self.accounts[alias].webfinger,
+                'key':self.accounts[alias].key,
+                'secret':self.accounts[alias].secret,
+                'token':self.accounts[alias].token,
+                'token_secret':self.accounts[alias].token_secret
+            }
+            self.writeCfg()
 
-    def doFollow(self):
-        for i in self.accounts['old'].uniquefollows:
-            person = self.accounts['new'].pump.Person(i)
-            print person.id
+        self.backupFollowing(self.accounts[alias])
+        print('%s (following %s) loaded as %s account\n----' % (self.cfg[webfinger]['webfinger'],
+                                                  len(self.accounts[alias].following),
+                                                  alias))
+
+    def followMany(self, acct, contacts):
+        for contact in contacts:
+            # we dont want to follow ourselves
+            if contact == acct.webfinger:
+                print(" Skipped: %s (your account)" % contact)
+            # skip people we are already following
+            elif contact in acct.following:
+                print(" Skipped: %s (already following)" % contact)
+            elif acct.follow(contact):
+                print(" Followed: %s" % contact)
+            else:
+                print(" Failed: %s" % contact)
+
+    def unfollowMany(self, acct, contacts):
+        for contact in contacts:
+            # skip contacts we are not following
+            if contact not in acct.following:
+                print(" Skipped %s (not following)" % contact)
+            elif acct.unfollow(contact):
+                print(" Unfollowed: %s" % contact)
+            else:
+                print(" Failed: %s" % contact)
+
+    def promptEnter(self, msg):
+        print(msg)
+        try:
+            raw_input("Hit enter to continue or ctrl+c to quit")
+        except KeyboardInterrupt:
+            sys.exit()
 
     def run(self):
-        self.getUniqueFollowing(self.account_names)
-        self.getUniqueFollowing(self.account_names[::-1])
-        #self.doFollow()
+        old = self.accounts['old']
+        new = self.accounts['new']
+
+        # follow contacts from old account
+        self.promptEnter("%s will now follow %s new contacts" % (new.webfinger, len(old.following)))
+        self.followMany(new, old.following)
+        new.getFollowing()
+        print("%s is now following %s contacts\n----" % (new.webfinger, len(new.following)))
+
+        # unfollow contacts followed by both old and new
+        oldandnew = [x for x in old.following if x in new.following]
+        self.promptEnter("%s will now unfollow %s contacts" % (old.webfinger, len(oldandnew)))
+        self.unfollowMany(old, oldandnew)
+        old.getFollowing()
+        print("%s is now following %s contacts\n----" % (old.webfinger, len(old.following)))
 
 if __name__ == '__main__':
     app = Client()
